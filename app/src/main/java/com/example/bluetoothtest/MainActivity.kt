1f449339808bdc77921bb9edea611a4dd7e58d26
Deimos <deimoshall@gmail.com>
2022-07-24 17:36:15 -0500

    Button switch led state added

--- app/src/main/java/com/example/bluetoothtest/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothtest/MainActivity.kt
@@ -6,6 +6,8 @@ import android.bluetooth.BluetoothAdapter
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.Switch
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -18,14 +20,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val switchBluetooth = findViewById<SwitchMaterial>(R.id.sw_bluetooth)
+        val btnLedState = findViewById<Button>(R.id.btn_led_state)
         val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
         switchBluetooth.isChecked = bluetoothAdapter!!.isEnabled
 
         switchBluetooth.setOnCheckedChangeListener { buttonView, isChecked ->
             when {
-                isChecked -> bluetoothAdapter.enable()
-                !isChecked -> bluetoothAdapter.disable()
+                isChecked -> {
+                    bluetoothAdapter.enable()
+                    btnLedState.visibility = View.VISIBLE
+                }
+                !isChecked -> {
+                    bluetoothAdapter.disable()
+                    btnLedState.visibility = View.INVISIBLE
+                }
+            }
+        }
+
+        btnLedState.setOnClickListener {
+            if (btnLedState.text.toString() == getString(R.string.turn_led_on)) {
+                btnLedState.text = getString(R.string.turn_led_off)
+            } else {
+                btnLedState.text = getString(R.string.turn_led_on)
             }
         }
     }
