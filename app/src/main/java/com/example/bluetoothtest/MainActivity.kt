d12d39095c14999fdb1cbeb08b285831854a7fa5
Deimos <deimoshall@gmail.com>
2022-07-24 21:24:54 -0500

    startActivityForResult changed to registerForActivityResult

--- app/src/main/java/com/example/bluetoothtest/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothtest/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.bluetoothtest
 import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,11 +14,13 @@ import android.view.View
 import android.widget.Button
 import android.widget.Switch
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import com.example.bluetoothtest.BuildConfig.DEBUG
 import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
+
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,22 +28,33 @@ class MainActivity : AppCompatActivity() {
 
         val switchBluetooth = findViewById<SwitchMaterial>(R.id.sw_bluetooth)
         val btnLedState = findViewById<Button>(R.id.btn_led_state)
-        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
 
-        switchBluetooth.isChecked = bluetoothAdapter!!.isEnabled
-        when (switchBluetooth.isChecked) {
+        val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            when (it.resultCode) {
+                RESULT_OK -> {
+                    btnLedState.visibility = View.VISIBLE
+                    switchBluetooth.isChecked = true
+                }
+                RESULT_CANCELED -> {
+                    btnLedState.visibility = View.INVISIBLE
+                    switchBluetooth.isChecked = false
+                }
+            }
+        }
+
+        switchBluetooth.isChecked = bluetoothAdapter!!.isEnabled // Init state for switchBluetooth
+        when (switchBluetooth.isChecked) {                      // Init state for btnLedState
             true -> btnLedState.visibility = View.VISIBLE
             false -> btnLedState.visibility = View.INVISIBLE
         }
 
-        Log.d("STATE", "Visible = ${View.VISIBLE.toString()}")
-        Log.d("STATE", "Invisible = ${View.INVISIBLE}")
-
         switchBluetooth.setOnCheckedChangeListener { buttonView, isChecked ->
             when {
                 isChecked -> {
-                    bluetoothAdapter.enable()
-                    btnLedState.visibility = View.VISIBLE
+                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    getResult.launch(enableBtIntent)
                 }
                 !isChecked -> {
                     bluetoothAdapter.disable()
