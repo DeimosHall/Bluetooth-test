1a2452c3c53a4626b9593eb6c41c843181c7403f
Deimos <deimoshall@gmail.com>
2022-07-26 15:03:09 -0500

    Try of MyBluetoothService

--- app/src/main/java/com/example/bluetoothtest/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothtest/MainActivity.kt
@@ -3,23 +3,31 @@ package com.example.bluetoothtest
 import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.Switch
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import com.example.bluetoothtest.BuildConfig.DEBUG
 import com.google.android.material.switchmaterial.SwitchMaterial
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val deviceName = "ESP32 board"
+    private var deviceAddress = ""
+
+    companion object {
+        val myUUID = UUID.fromString("d3f7b72b-3f98-4c98-8a79-d0d55603b737")
+        val bluetoothSocket : BluetoothSocket? = null
+        //lateinit var bluetoothAdapter: BluetoothAdapter
+
+    }
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +38,8 @@ class MainActivity : AppCompatActivity() {
         val btnLedState = findViewById<Button>(R.id.btn_led_state)
         val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
         val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+        val handler: Handler? = null
+        val blueService = MyBluetoothService(handler!!)
 
         val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             when (it.resultCode) {
@@ -44,8 +54,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        switchBluetooth.isChecked = bluetoothAdapter!!.isEnabled // Init state for switchBluetooth
-        when (switchBluetooth.isChecked) {                      // Init state for btnLedState
+        val pairedDevicesList: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+        for (device in pairedDevicesList!!) {
+            Log.d("CHECK", "${device.name}: ${device.address}")
+            if (device.name == deviceName) {
+                deviceAddress = device.address
+            }
+        }
+
+        // Init state for switchBluetooth
+        switchBluetooth.isChecked = bluetoothAdapter.isEnabled
+        // Init state for btnLedState
+        when (switchBluetooth.isChecked) {
             true -> btnLedState.visibility = View.VISIBLE
             false -> btnLedState.visibility = View.INVISIBLE
         }
@@ -64,6 +84,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnLedState.setOnClickListener {
+            // Changes the text of the button
             if (btnLedState.text.toString() == getString(R.string.turn_led_on)) {
                 btnLedState.text = getString(R.string.turn_led_off)
             } else {
